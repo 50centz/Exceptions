430cb390ffe2dfcf8f6fb26c0b73616bb4b1da9e
Федоров Евгений Дом ПК <zen-g@mail.ru>
2023-06-24 16:22:01 +0300

    Git init

---
+++ HomeWork1/Task1.java
@@ -0,0 +1,21 @@
+package HomeWorks.HomeWork1;
+
+/**
+ * Task1
+ */
+public class Task1 {
+
+    public static void main(String[] args) {
+        division(10, 0);
+        
+    }
+
+
+    private static void division(int a, int b){
+        try {
+            
+        } catch (ArithmeticException e) {
+            System.out.println("Деление на ноль нельзя");
+        }
+    }
+}
